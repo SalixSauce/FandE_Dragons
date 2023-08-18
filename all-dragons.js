eed16f5106415d8618aff19d4350e714fee3fa3b
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2023-08-17 21:00:21 -0400

    almost done with phase 4

--- all-dragons.js
+++ all-dragons.js
@@ -1,4 +1,19 @@
-// Your code here
+const FriendlyDragon = require('./classes/friendly-dragon')
+const EvilDragon = require('./classes/evil-dragon')
+
+const falkor = new FriendlyDragon(
+  'falkor',
+  'white',
+  ["save Atreyu from the swamp", "save Atreyu from the Nothing", "scare the local bullies into a dumpster"],
+  "Bastian",
+)
+
+const smaug = new EvilDragon(
+  "Smaug",
+  "black",
+  ["take over your mountain kingdom", "steal all your dwarven gold", "burn down your floating village"],
+  "Dwarf King"
+)
 
 
 /****************************************************************************/
