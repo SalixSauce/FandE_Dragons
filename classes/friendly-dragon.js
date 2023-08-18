007f5be448aeba1e473307cc635a351c353f4c2d
Sydney Barnes-Grant <sydneybarnesg@gmail.com>
2023-08-17 20:13:26 -0400

    phase one complete

--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,24 @@
-// Your code here
+const Dragon = require('./dragon')
+
+class FriendlyDragon extends Dragon {
+    constructor (name, color, lifeGoals, friend) {
+        super(name, color)
+        this.lifeGoals = lifeGoals
+        this.friend = friend
+
+    }
+
+    hasLifeGoals() {
+        this.lifeGoals.forEach( e => {
+            return `${this.name} likes to ${this.lifeGoals}.`
+        })
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${friend}.`
+    }
+
+
+}
+
+module.exports = FriendlyDragon
